ae736d5dd12b63f98088f1df4ed91a32e4141f9b
unknown <albertsbredikis@gmail.com>
2022-01-29 13:22:26 +0200

    Added font

---
+++ src/components/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h4></h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      title: "Happy Valentine’s Day!",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+h1 {
+  font-family: "Dancing Script", cursive;
+  font-size: 9.188rem;
+}
+</style>
